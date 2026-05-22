e222d52da4ab2f71532655cd7a6e8b238bb080c5
Roberto Obando <robertoandres24@gmail.com>
2026-05-22 16:14:56 -0400

    adding flags

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,69 @@
 import { useState, useMemo, useEffect } from 'react'
 import { useI18n } from './hooks/useI18n.js'
 import curiosities from './curiosities.json'
+import flags from './flags.js'
 
 const stickersData = [
   // Grupo A
-  { page: 8, code: 'MEX', name: 'Mexico', group: 'A' },
-  { page: 10, code: 'RSA', name: 'South Africa', group: 'A' },
-  { page: 12, code: 'KOR', name: 'Korea Republic', group: 'A' },
-  { page: 14, code: 'CZE', name: 'Czechia', group: 'A' },
+  { page: 8, code: 'MEX', name: 'Mexico', group: 'A', iso: 'mx' },
+  { page: 10, code: 'RSA', name: 'South Africa', group: 'A', iso: 'za' },
+  { page: 12, code: 'KOR', name: 'Korea Republic', group: 'A', iso: 'kr' },
+  { page: 14, code: 'CZE', name: 'Czechia', group: 'A', iso: 'cz' },
   // Grupo B
-  { page: 16, code: 'CAN', name: 'Canada', group: 'B' },
-  { page: 18, code: 'BIH', name: 'Bosnia & Herzegovina', group: 'B' },
-  { page: 20, code: 'QAT', name: 'Qatar', group: 'B' },
-  { page: 22, code: 'SUI', name: 'Switzerland', group: 'B' },
+  { page: 16, code: 'CAN', name: 'Canada', group: 'B', iso: 'ca' },
+  { page: 18, code: 'BIH', name: 'Bosnia & Herzegovina', group: 'B', iso: 'ba' },
+  { page: 20, code: 'QAT', name: 'Qatar', group: 'B', iso: 'qa' },
+  { page: 22, code: 'SUI', name: 'Switzerland', group: 'B', iso: 'ch' },
   // Grupo C
-  { page: 24, code: 'BRA', name: 'Brazil', group: 'C' },
-  { page: 26, code: 'MAR', name: 'Morocco', group: 'C' },
-  { page: 28, code: 'HAI', name: 'Haiti', group: 'C' },
-  { page: 30, code: 'SCO', name: 'Scotland', group: 'C' },
+  { page: 24, code: 'BRA', name: 'Brazil', group: 'C', iso: 'br' },
+  { page: 26, code: 'MAR', name: 'Morocco', group: 'C', iso: 'ma' },
+  { page: 28, code: 'HAI', name: 'Haiti', group: 'C', iso: 'ht' },
+  { page: 30, code: 'SCO', name: 'Scotland', group: 'C', iso: 'gb-sct' },
   // Grupo D
-  { page: 32, code: 'USA', name: 'USA', group: 'D' },
-  { page: 34, code: 'PAR', name: 'Paraguay', group: 'D' },
-  { page: 36, code: 'AUS', name: 'Australia', group: 'D' },
-  { page: 38, code: 'TUR', name: 'Turkey', group: 'D' },
+  { page: 32, code: 'USA', name: 'USA', group: 'D', iso: 'us' },
+  { page: 34, code: 'PAR', name: 'Paraguay', group: 'D', iso: 'py' },
+  { page: 36, code: 'AUS', name: 'Australia', group: 'D', iso: 'au' },
+  { page: 38, code: 'TUR', name: 'Turkey', group: 'D', iso: 'tr' },
   // Grupo E
-  { page: 40, code: 'GER', name: 'Germany', group: 'E' },
-  { page: 42, code: 'CUW', name: 'Curacao', group: 'E' },
-  { page: 44, code: 'CIV', name: "Cote d'Ivoire", group: 'E' },
-  { page: 46, code: 'ECU', name: 'Ecuador', group: 'E' },
+  { page: 40, code: 'GER', name: 'Germany', group: 'E', iso: 'de' },
+  { page: 42, code: 'CUW', name: 'Curacao', group: 'E', iso: 'cw' },
+  { page: 44, code: 'CIV', name: "Cote d'Ivoire", group: 'E', iso: 'ci' },
+  { page: 46, code: 'ECU', name: 'Ecuador', group: 'E', iso: 'ec' },
   // Grupo F
-  { page: 48, code: 'NED', name: 'Netherlands', group: 'F' },
-  { page: 50, code: 'JPN', name: 'Japan', group: 'F' },
-  { page: 52, code: 'SWE', name: 'Sweden', group: 'F' },
-  { page: 54, code: 'TUN', name: 'Tunisia', group: 'F' },
+  { page: 48, code: 'NED', name: 'Netherlands', group: 'F', iso: 'nl' },
+  { page: 50, code: 'JPN', name: 'Japan', group: 'F', iso: 'jp' },
+  { page: 52, code: 'SWE', name: 'Sweden', group: 'F', iso: 'se' },
+  { page: 54, code: 'TUN', name: 'Tunisia', group: 'F', iso: 'tn' },
   // Grupo G
-  { page: 58, code: 'BEL', name: 'Belgium', group: 'G' },
-  { page: 60, code: 'EGY', name: 'Egypt', group: 'G' },
-  { page: 62, code: 'IRN', name: 'IR Iran', group: 'G' },
-  { page: 64, code: 'NZL', name: 'New Zealand', group: 'G' },
+  { page: 58, code: 'BEL', name: 'Belgium', group: 'G', iso: 'be' },
+  { page: 60, code: 'EGY', name: 'Egypt', group: 'G', iso: 'eg' },
+  { page: 62, code: 'IRN', name: 'IR Iran', group: 'G', iso: 'ir' },
+  { page: 64, code: 'NZL', name: 'New Zealand', group: 'G', iso: 'nz' },
   // Grupo H
-  { page: 66, code: 'ESP', name: 'Spain', group: 'H' },
-  { page: 68, code: 'CPV', name: 'Cape Verde', group: 'H' },
-  { page: 70, code: 'KSA', name: 'Saudi Arabia', group: 'H' },
-  { page: 72, code: 'URU', name: 'Uruguay', group: 'H' },
+  { page: 66, code: 'ESP', name: 'Spain', group: 'H', iso: 'es' },
+  { page: 68, code: 'CPV', name: 'Cape Verde', group: 'H', iso: 'cv' },
+  { page: 70, code: 'KSA', name: 'Saudi Arabia', group: 'H', iso: 'sa' },
+  { page: 72, code: 'URU', name: 'Uruguay', group: 'H', iso: 'uy' },
   // Grupo I
-  { page: 74, code: 'FRA', name: 'France', group: 'I' },
-  { page: 76, code: 'SEN', name: 'Senegal', group: 'I' },
-  { page: 78, code: 'IRQ', name: 'Iraq', group: 'I' },
-  { page: 80, code: 'NOR', name: 'Norway', group: 'I' },
+  { page: 74, code: 'FRA', name: 'France', group: 'I', iso: 'fr' },
+  { page: 76, code: 'SEN', name: 'Senegal', group: 'I', iso: 'sn' },
+  { page: 78, code: 'IRQ', name: 'Iraq', group: 'I', iso: 'iq' },
+  { page: 80, code: 'NOR', name: 'Norway', group: 'I', iso: 'no' },
   // Grupo J
-  { page: 82, code: 'ARG', name: 'Argentina', group: 'J' },
-  { page: 84, code: 'ALG', name: 'Algeria', group: 'J' },
-  { page: 86, code: 'AUT', name: 'Austria', group: 'J' },
-  { page: 88, code: 'JOR', name: 'Jordan', group: 'J' },
+  { page: 82, code: 'ARG', name: 'Argentina', group: 'J', iso: 'ar' },
+  { page: 84, code: 'ALG', name: 'Algeria', group: 'J', iso: 'dz' },
+  { page: 86, code: 'AUT', name: 'Austria', group: 'J', iso: 'at' },
+  { page: 88, code: 'JOR', name: 'Jordan', group: 'J', iso: 'jo' },
   // Grupo K
-  { page: 90, code: 'POR', name: 'Portugal', group: 'K' },
-  { page: 92, code: 'COD', name: 'Congo DR', group: 'K' },
-  { page: 94, code: 'UZB', name: 'Uzbekistan', group: 'K' },
-  { page: 96, code: 'COL', name: 'Colombia', group: 'K' },
+  { page: 90, code: 'POR', name: 'Portugal', group: 'K', iso: 'pt' },
+  { page: 92, code: 'COD', name: 'Congo DR', group: 'K', iso: 'cd' },
+  { page: 94, code: 'UZB', name: 'Uzbekistan', group: 'K', iso: 'uz' },
+  { page: 96, code: 'COL', name: 'Colombia', group: 'K', iso: 'co' },
   // Grupo L
-  { page: 98, code: 'ENG', name: 'England', group: 'L' },
-  { page: 100, code: 'CRO', name: 'Croatia', group: 'L' },
-  { page: 102, code: 'GHA', name: 'Ghana', group: 'L' },
-  { page: 104, code: 'PAN', name: 'Panama', group: 'L' },
+  { page: 98, code: 'ENG', name: 'England', group: 'L', iso: 'gb-eng' },
+  { page: 100, code: 'CRO', name: 'Croatia', group: 'L', iso: 'hr' },
+  { page: 102, code: 'GHA', name: 'Ghana', group: 'L', iso: 'gh' },
+  { page: 104, code: 'PAN', name: 'Panama', group: 'L', iso: 'pa' },
 ]
 
 // Create a map for O(1) lookup of curiosities by country code
@@ -360,6 +361,7 @@ function App() {
               style={{ cursor: 'pointer' }}
             >
               <div className="page-number">{sticker.page}</div>
+              <img src={flags[sticker.iso]} alt={sticker.name} className="country-flag" />
               <div className="country-info">
                 <div className="country-code">{sticker.code}</div>
                 <div className="country-name">{sticker.name}</div>
